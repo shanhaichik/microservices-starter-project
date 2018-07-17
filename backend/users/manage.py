61ff969a9afcfcdcca63b87ebdcc6e89c071912a
Dukhovnyak Aleksandr Gennadyevich <a.dukhovnyak@tinkoff.ru>
2018-07-17 17:39:51 +0300

    add tests + code coverage

--- backend/users/manage.py
+++ backend/users/manage.py
@@ -1,4 +1,5 @@
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 from project import create_app, db
@@ -7,12 +8,24 @@ from project.api.models import User
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+
+
 # reset db command
 @cli.command()
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 # test run comand
 @cli.command()
@@ -24,6 +37,7 @@ def test():
         return 0
     return 1
 
+
 # add test data
 @cli.command()
 def seed_db():
@@ -31,6 +45,22 @@ def seed_db():
     db.session.add(User(username='sasha', email="test1@gmail.com"))
     db.session.add(User(username='petya', email="test2@mherman.org"))
     db.session.commit()
+
+# code coverage
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
--- backend/users/project/api/users.py
+++ backend/users/project/api/users.py
@@ -1,10 +1,20 @@
 from sqlalchemy import exc
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from project import db
 from project.api.models import User
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 @users_blueprint.route('/users/ping', methods=['GET'])
 def ping():
